07dd10ee1f55392268abb48b0af92368344e14f5
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-25 15:04:08 +0800

    feat: added BrowserRouter and bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 import { Routes } from "./Routes";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

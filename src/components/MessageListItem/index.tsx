daea87dc81b5b0c9be56b4786ec7e9545859f0cf
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-29 01:29:26 +0800

    feat: added isRead and id

--- src/components/MessageListItem/index.tsx
+++ src/components/MessageListItem/index.tsx
@@ -1,17 +1,21 @@
 import React, { FunctionComponent } from "react";
 
-export const MessageListItem: FunctionComponent<MessageListItemProps> = ({title, message, email, createdAt}) => (
-  <tr>
-    <td>{createdAt}</td>
-    <td>{email}</td>
-    <td>{title}</td>
-    <td>{message}</td>
+export const MessageListItem: FunctionComponent<MessageListItemProps> = ({id, title, message, email, createdAt, isRead}) => (
+  <tr style={{backgroundColor: isRead ? "#999999" : "#FFFFFF"}}>
+    <a href={`/messages/${id}`}>
+      <td>{createdAt}</td>
+      <td>{email}</td>
+      <td>{title}</td>
+      <td>{message}</td>
+    </a>
   </tr>
 );
 
 export interface MessageListItemProps {
+  readonly id: string;
   readonly title: string;
   readonly message: string;
   readonly email: string;
   readonly createdAt: string;
+  readonly isRead: string;
 }
